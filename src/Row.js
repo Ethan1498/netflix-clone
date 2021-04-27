1763f9918b7388dd49238f7b3865e7d13cd72185
Ethan Gough <dante1498@gmail.com>
2021-04-27 19:33:42 +0100

    Change api request to pull netflix originals, set css file for image size

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "./axios";
-import './Row.css';
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -27,7 +27,7 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => ( 
           <img
-            classname="row__poster"
+            className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
           />
