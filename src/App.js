cc294c5dd855a757f585c2271bb85f8eed22e430
Ethan Gough <dante1498@gmail.com>
2021-04-28 19:44:10 +0100

    Create smaller rows for non netflix originals

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
