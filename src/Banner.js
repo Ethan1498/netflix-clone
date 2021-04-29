db4feddd90dbaec5d09eefbd7b6acc99b3315578
Ethan Gough <dante1498@gmail.com>
2021-04-29 20:20:52 +0100

    Finish styling the banner, and move body colour to black to fit netflix

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -20,28 +21,27 @@ function Banner() {
 
     console.log(movie);
 
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
+
     return (
         <header className="banner"
             style={{
                 backgroundSize: "cover",
-                backgroundImage: `url{
-                    "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
-                }`,
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
                 backgroundPosition: "center center",
             }}
         >
             <div className="div banner__contents">
-                {/* <<< background image */}
-                <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-                <div className="div banner__buttons">
-                    <button className="banner__button">
-                        Play
-                    </button>
-                    <button className="banner__button">
-                        My List
-                    </button>
+                <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
                 </div>
-                {/* desc */}
+                <h1 className="banner__description">
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
         </header>
     )
